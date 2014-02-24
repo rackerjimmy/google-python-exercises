dc927aa19502e5d6fa93554c99c08e2d19469d17
Jimmy Prescott <jimmy.prescott@rackspace.com>
2014-02-23 22:00:51 -0600

    test

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
